0d08729ba15e1349bd6d61d753285129ff5ab758
Pratyush Dhungana <p.dhungana5701@gmail.com>
2025-06-06 19:59:39 -0500

    Update php-email-form.php
    update email for response

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -13,7 +13,7 @@ if(isset($_POST['submit'])) {
   $message = trim($message);
 
   // Send email
-  $to = 'pd624@msstate.edu';
+  $to = 'dh.prtsh@gmail.com';
   $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n" .
              'Reply-To: ' . $email . "\r\n" .
              'X-Mailer: PHP/' . phpversion();
